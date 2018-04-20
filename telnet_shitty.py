1f51207e6e59a7394829dbdbf3038383177b2829
fostrb <fostrb@gmail.com>
2018-04-19 22:28:10 -0400

    Fixed dumb telnet client commit.

--- telnet_shitty.py
+++ telnet_shitty.py
@@ -26,7 +26,6 @@ def run():
             else:
                 #line = sys.stdin.readline().encode('ascii')
                 line = input().encode('ascii')
-                line = readline.get
                 line += b'\n'
                 strline = line.decode('ascii').strip('\n')
                 if not line:
